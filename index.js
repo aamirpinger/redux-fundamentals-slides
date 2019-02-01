30edb03bc6e0eab3780dc6cdd6651ce0003d9042
Aamir Pinger <aamirpinger@yahoo.com>
2019-02-01 19:34:39 +0500

    Added index.js file
    this index.js contains all the code from the react + redux slides by Aamir Pinger

---
+++ index.js
@@ -0,0 +1,144 @@
+function createStore(reducer) {
+  // The store should have four parts
+  // 1. The state
+  // 2. Get the state
+  // 3. Listen to changes on the state
+  // 4. Update the state
+
+
+  let state
+  let listeners = []
+
+  /*
+  // this subscribe function will only add listner to listerners array
+  const subscribe = (listener) => {
+    listeners.push(listener)
+  }
+*/
+
+  // this subscribe function will add listner to listerners array and return a callback
+  // to unsubscribe (remove that listener from listeners array)
+  const subscribe = (listener) => {
+    listeners.push(listener)
+    return () => {
+      listeners = listeners.filter((l) => l !== listener)
+    }
+  }
+
+  // getState will simply return the state
+  const getState = () => state
+
+  // Purpose of dispatch funtion is to triger state change by
+  // forwarding action to reducers function, second part of this function is to
+  // invoke all the listener function add in listeners array
+  const dispatch = (action) => {
+    state = reducer(state, action)
+    listeners.forEach(listener => listener())
+  }
+
+  return {
+    getState,
+    subscribe,
+    dispatch
+  }
+}
+
+function task_reducer(state = [], action) {
+  // this function will cater all the action related to ToDo Tasks
+
+  switch (action.type) {
+    // This switch case will check for incoming action's type and perform task accordingly
+    case 'DELETE_TASK':
+      return state.filter((r) => r.id !== action.id)
+    case 'TOGGLE_STATUS':
+      return state.map((r) =>
+        (r.id === action.id)
+          ? Object.assign({}, r, { task_done: !r.task_done })
+          : r)
+    case 'CREATE_TASK':
+      return state.concat([action.task])
+    default:
+      return state
+  }
+}
+
+
+function movie_reducer(state = [], action) {
+  // this function will cater all the action related to Movies list
+
+  switch (action.type) {
+    // This switch case will check for incoming action's type and perform task accordingly
+    case 'ADD_MOVIE':
+      return state.concat([action.movie])
+    case 'REMOVE_MOVIE':
+      return state.filter((r) => r.id !== action.id)
+    case 'MOVIE_WATCHED':
+      return state.map((r) => (r.id === action.id) ? Object.assign({}, r, { watched: !r.watched }) : r)
+    default:
+      return state
+  }
+}
+
+
+function root_reducer(state = [], action) {
+  // this function will hold all the reducers and only this will be passed to store as
+  // reducer function. Whenever dispatch function in store will call reducer function
+  // all the reducer function that is listed in this function will get invoked
+
+  return {
+    task: task_reducer(state.task, action),
+    movie: movie_reducer(state.movie, action),
+  }
+
+
+}
+
+
+// this will create store and pass root_reducer as universal reducer to it
+const store = createStore(root_reducer)
+
+
+// this is how we can subscribe any listener, invoking unsubscribe() afterward will remove this 
+// listener from listeners array
+unsubscribe = store.subscribe(() => console.log('Show me the current state', store.getState()))
+
+
+//this is the way to discpatch any action to store, store then forward 
+//this aciton and state to reducer
+store.dispatch({
+  type: 'ADD_MOVIE',
+  movie: {
+    id: "0",
+    name: "Mission Impossible",
+    watched: false
+  }
+
+})
+
+store.dispatch({
+  type: 'ADD_MOVIE',
+  movie: {
+    id: "1",
+    name: "Transporter",
+    watched: false
+  }
+
+})
+
+store.dispatch({
+  type: 'MOVIE_WATCHED',
+  id: "0",
+})
+
+
+store.dispatch({
+  type: 'CREATE_TASK',
+  task: {
+    id: "0",
+    name: "Jogging",
+    description: "Going Jogging 3 km 7 in the morning"
+  }
+
+
+})
+
